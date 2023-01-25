c390c5c48f8800415f39ac13e7a09bad2243ce28
jeremy koundour <j.koundour@gmail.com>
2023-01-25 15:27:01 +0100

    création des pages // mise en place des diff routes vers les pages

---
+++ src/components/APropos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function APropos() {
+  return (
+    <div>
+      <h1>À Propos</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './pages/App.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
