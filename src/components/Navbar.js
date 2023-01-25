8b035427ba1d9a3b7a31977e13de4f7b365231a4
jeremy koundour <j.koundour@gmail.com>
2023-01-25 17:00:39 +0100

    mise en place navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav>
+      <img src={'../assets/LOGO.png'} alt="logoKasa" />
       <Link to="/">Accueil</Link>
       <Link to="/APropos">À Propos</Link>
     </nav>
