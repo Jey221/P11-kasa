9705c1b07ca97b5dbe5b0060d46e4797c4164d00
jeremy koundour <j.koundour@gmail.com>
2023-01-30 18:59:11 +0100

    mise en place page 404

--- src/pages/Erreur.js
+++ src/pages/Erreur.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from '../style/Erreur.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Erreur() {
   return (
-    <div>
-      <h1>Erreur 404</h1>
+    <div className={styles.errorPage}>
+      <h1>404</h1>
+      <h2>Oups! La page que vous demandez n'existe pas</h2>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 }
