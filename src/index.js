6345a5b7b60fee0d9934375e294a635a78e8a0f0
jeremy koundour <j.koundour@gmail.com>
2023-02-10 18:48:58 +0100

    mise en place de React 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
